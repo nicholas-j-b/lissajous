45271bab35b8338a79bdd7442c7475d738818ad8
nicholas <nicholas.j.brooking@gmail.com>
2020-02-25 23:40:54 +0100

    canvas resize

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 class Config {
     static NUM_COLS = 14;
     static NUM_ROWS = 12;
-    static VERTICAL_SPACING = 60;
-    static HORIZONTAL_SPACING = 60;
-    static CANVAS_WIDTH = 1000;
-    static CANVAS_HEIGHT = 800;
+    static VERTICAL_SPACING = 70;
+    static HORIZONTAL_SPACING = 70;
+    static CANVAS_WIDTH = 1400;
+    static CANVAS_HEIGHT = 1000;
     static LINE_WIDTH = 1;
     static COLOURS = {
         lines: '#FFFFFF0F',
         background: '#003030'
     };
     static ANIMATION = {
-        speed: 0.01,
-        fps: 20
+        speed: 0.005,
+        fps: 30
     };
-    static FLUCTUATION = 20;
+    static FLUCTUATION = 30;
 }
 
 class Timer {
